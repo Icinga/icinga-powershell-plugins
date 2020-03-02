5cff8f7cd0dfa0a70a4776b787fb0a9f6c832c31
Crited <45196568+Crited@users.noreply.github.com>
2020-03-02 15:35:21 +0100

    Update Certificate Provider
    Add CertStorePath object - now not forcing space 'my'; fix codestyling

--- provider/certificate/Icinga_ProviderCertificate.psm1
+++ provider/certificate/Icinga_ProviderCertificate.psm1
@@ -1,30 +1,31 @@
 function Get-IcingaCertificateData()
 {
    param(
-    #CertStore-Related Param
-	  [ValidateSet('LocalMachine', 'CurrentUser')]
-	  [string]$CertStore     = $null,
-	  [array]$CertThumbprint = $null,
-	  [array]$CertSubject    = $null,
-	#Local Certs
+      #CertStore-Related Param
+      [ValidateSet('*', 'LocalMachine', 'CurrentUser')]
+      [string]$CertStore     = '*',
+      [array]$CertThumbprint = $null,
+      [array]$CertSubject    = $null,
+      $CertStorePath         = '*',
+      #Local Certs
       [array]$CertPaths      = $null,
-	  [array]$CertName		 = $null
+      [array]$CertName	     = $null
+
    );
    [hashtable]$CertData = @{};
    
    if ([string]::IsNullOrEmpty($CertStore) -eq $FALSE){
-      $CertDataStore = Get-IcingaCertStoreCertificates -CertStore $CertStore -CertThumbprint $CertThumbprint -CertSubject $CertSubject
+      $CertDataStore = Get-IcingaCertStoreCertificates -CertStore $CertStore -CertThumbprint $CertThumbprint -CertSubject $CertSubject -CertStorePath $CertStorePath;
    }
    
    if (($null -ne $CertPaths) -or ($null -ne $CertName)) {
       $CertDataFile = Get-IcingaDirectoryRecurse -Path $CertPaths -FileNames $CertName;
-	  Write-Host "HI"
    }
    
    if ($null -ne $CertDataFile) {
       foreach ($Cert in $CertDataFile) {
          $CertConverted = New-Object Security.Cryptography.X509Certificates.X509Certificate2 $Cert.FullName;
-	     $CertDataFile = $CertConverted;
+	    $CertDataFile = $CertConverted;
       }
    }
 
@@ -37,30 +38,35 @@ function Get-IcingaCertificateData()
 function Get-IcingaCertStoreCertificates()
 {
    param(
-    #CertStore-Related Param
-	  [ValidateSet('LocalMachine', 'CurrentUser')]
-	  [string]$CertStore = 'LocalMachine',
-	  [array]$CertThumbprint = $null,
-	  [array]$CertSubject    = $null
-	);
+   #CertStore-Related Param
+   [ValidateSet('*', 'LocalMachine', 'CurrentUser')]
+   [string]$CertStore = '*',
+   [array]$CertThumbprint = $null,
+   [array]$CertSubject    = $null,
+   $CertStorePath         = '*'
+   );
 
    $CertStoreArray = @();
-   $CertStorePath  = [string]::Format('Cert:\{0}\My', $CertStore)
+   $CertStorePath  = [string]::Format('Cert:\{0}\{1}', $CertStore, $CertStorePath);
    $CertStoreCerts = Get-ChildItem -Path $CertStorePath;
+   
+   if ($CertSubject -eq $null) {
+      $CertSubject += '*'
+   }
    
    if ($CertSubject -ne $null) {
       foreach ($Subject in $CertSubject)
       {
-	     $CertStoreArray += Get-ChildItem -Path $CertStorePath | Where-Object { $_.Subject -like $Subject }
+         $CertStoreArray += Get-ChildItem -Path $CertStorePath -Recurse | Where-Object { $_.Subject -like $Subject };
       }
    }
    
    if ($CertThumbprint -ne $null) {
       foreach ($Thumbprint in $CertThumbprint)
       {
-	     if ($CertStoreArray.Thumbprint -like ($Thumbprint)) {
-	        $CertStoreArray += Get-ChildItem -Path $CertStorePath | Where-Object { $_.Thumbprint -like $Thumbprint }
-		 }
+         if ($CertStoreArray.Thumbprint -like ($Thumbprint)) {
+            $CertStoreArray += Get-ChildItem -Path $CertStorePath -Recurse | Where-Object { $_.Thumbprint -like $Thumbprint };
+         }
       }   
    }
    
