f8a23b6095183a3f6676b6ab2afe2568a12414c3
Crited <45196568+Crited@users.noreply.github.com>
2020-03-05 10:51:45 +0100

    Remove redundant code

--- plugins/Invoke-IcingaCheckCertificate.psm1
+++ plugins/Invoke-IcingaCheckCertificate.psm1
@@ -16,20 +16,16 @@
    | 'certificate_cncloudbaseinit_winrmacacbac2a29adc68d710c715da20b036407ba3a8'=313784201.23;@864000:31536000;@0:864000
    0
 .EXAMPLE
-   PS> Invoke-IcingaCheckCertificate -CertStore 'LocalMachine' -CertStorePath 'My' -CertThumbprint '*' -WarningStart '@0d:10d' -Verbosity 3
-   [OK] Check package "Certificates" (Match All)
-   \_ [OK] Check package "Certificate Start" (Match All)
-      \_ [OK] Certificate CN=Cloudbase-Init WinRM(ACACBAC2A29ADC68D710C715DA20B036407BA3A8): -1662338.9726846
-   | 'certificate_cncloudbaseinit_winrmacacbac2a29adc68d710c715da20b036407ba3a8'=-1662338.9726846;@0:864000;
-   0
+   PS> Invoke-IcingaCheckCertificate -CertStore 'LocalMachine' -CertStorePath 'My' -CertThumbprint '*'-CertPaths "C:\ProgramData\icinga2\var\lib\icinga2\certs" -CertName '*.crt' -WarningEnd '@0d:10000d' -Verbosity 3
+   [WARNING] Check package "Certificates" (Match All) - [WARNING] Certificate astoll-powershe(AD647B1AC4EF5B91F7261A7EE517C418844D7756), Certificate Cloudbase-Init WinRM(ACACBAC2A29ADC68D710C715DA20B036407BA3A8)
+   \_ [WARNING] Check package "Certificate End" (Match All)
+   \_ [WARNING] Certificate test-server(AD147B1AC4EF5B91F7261A7EE517C418844D7756): Value "471323842.25" is between threshold "0:864000000"
+   \_ [WARNING] Certificate Cloudbase-Init WinRM(ACACBAC1A29ADC68D710C715DA20B036407BA3A8): Value "313538391.82" is between threshold "0:864000000"
 .EXAMPLE
-   PS> Invoke-IcingaCheckCertificate -CertStore 'LocalMachine' -CertStorePath 'My' -CertThumbprint '*' -WarningEnd '@10d:365d' -CriticalEnd '@0d:10d' -Verbosity 3;
-   [CRITICAL] Check package "Certificates" (Match All) - [CRITICAL] Certificate CN=Cloudbase-Init WinRM(ACACBAC2A29ADC68D710C715DA20B036407BA3A8)
-   \_ [CRITICAL] Certificate CN=Cloudbase-Init WinRM(ACACBAC2A29ADC68D710C715DA20B036407BA3A8): Value "False" is not matching threshold "True"
-   \_ [OK] Check package "Certificate End" (Match All)
-      \_ [OK] Certificate CN=Cloudbase-Init WinRM(ACACBAC2A29ADC68D710C715DA20B036407BA3A8): 313783999.12
-   | 'certificate_cncloudbaseinit_winrmacacbac2a29adc68d710c715da20b036407ba3a8'=313783999.12;@864000:31536000;@0:864000 'certificate_cncloudbaseinit_winrmacacbac2a29adc68d710c715da20b036407ba3a8'=False;;True
-   2
+   PS> Invoke-IcingaCheckCertificate -CertStore 'LocalMachine' -CertStorePath 'My' -CertThumbprint '*'-CertPaths "C:\ProgramData\icinga2\var\lib\icinga2\certs" -CertName '*.crt' -Verbosity 3 -Trusted
+   [CRITICAL] Check package "Certificates" (Match All) - [CRITICAL] Certificate astoll-powershe(AD647B1AC4EF5B91F7261A7EE517C418844D7756), Certificate Cloudbase-Init WinRM(ACACBAC2A29ADC68D710C715DA20B036407BA3A8)
+   \_ [CRITICAL] Certificate astoll-powershe(AD647B1AC4EF5B91F7261A7EE517C418844D7756): Value "False" is not matching threshold "True"
+   \_ [CRITICAL] Certificate Cloudbase-Init WinRM(ACACBAC2A29ADC68D710C715DA20B036407BA3A8): Value "False" is not matching threshold "True"
 .PARAMETER Trusted
    Used to switch on trusted behavior. Whether to check, If the certificate is trusted by the system root.
    Will return Critical in case of untrust.
@@ -81,94 +77,49 @@
 function Invoke-IcingaCheckCertificate()
 {
    param(
-      #Checking
+   #Checking
       [switch]$Trusted,
       $WarningStart          = $null,
       $CriticalStart         = $null,
       $WarningEnd            = $null,
       $CriticalEnd           = $null,
       #CertStore-Related Param
-      [ValidateSet('*', 'LocalMachine', 'CurrentUser')]
-      [string]$CertStore     = '*',
+      [ValidateSet('*', 'LocalMachine', 'CurrentUser', $null)]
+      [string]$CertStore     = $null,
       [array]$CertThumbprint = $null,
       [array]$CertSubject    = $null,
       $CertStorePath         = '*',
       #Local Certs
       [array]$CertPaths      = $null,
-      [array]$CertName       = $null,
+      [array]$CertName		 = $null,
       #Other
       [ValidateSet(0, 1, 2, 3)]
-      [int]$Verbosity        = 0,
-      [switch]$NoPerfData
+      [int]$Verbosity        = 0
    );
 
    $CertData    = (Get-IcingaCertificateData -CertStore $CertStore -CertThumbprint $CertThumbprint -CertSubject $CertSubject -CertPaths $CertPaths -CertName $CertName -CertStorePath $CertStorePath);
    $CertPackage = New-IcingaCheckPackage -Name 'Certificates' -OperatorAnd -Verbose $Verbosity;
-   $Date        = Get-Date; 
+   
+   if (($null -ne $WarningStart) -Or ($null -ne $CriticalStart)) {
+      $CertPackageStart = New-IcingaCheckPackage -Name 'Certificate Start' -OperatorAnd -Verbose $Verbosity;
+   }
+   if (($null -ne $WarningEnd) -Or ($null -ne $CriticalEnd)) {
+      $CertPackageEnd   = New-IcingaCheckPackage -Name 'Certificate End' -OperatorAnd -Verbose $Verbosity;
+   }
 
-   if($Trusted) {
-      foreach($Cert in $CertData.CertStore) {
-         if ($Cert.Thumbprint -eq $CompareThumbprint) {
-	    continue;
-	 }
-	 $CompareThumbprint = $Cert.Thumbprint;
-         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject, $Cert.Thumbprint)) -Value (Test-Certificate $Cert -ErrorAction silentlycontinue);
-	 $IcingaCheck.CritIfNotMatch($TRUE) | Out-Null;
- 	 $CertPackage.AddCheck($IcingaCheck);
-      }
-      foreach($Cert in $CertData.CertFile) {
-         if ($Cert.Thumbprint -eq $CompareThumbprint) {
-            continue;
-         }
-         $CompareThumbprint = $Cert.Thumbprint;
-         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject, $Cert.Thumbprint)) -Value (Test-Certificate $Cert -ErrorAction silentlycontinue);
+   foreach($Cert in ([array]$CertData.CertStore + [array]$CertData.CertFile)) {
+      if ($Trusted) {
+         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject.Substring(3).Split(",")[0], $Cert.Thumbprint)) -Value (Test-Certificate $Cert -ErrorAction silentlycontinue);
          $IcingaCheck.CritIfNotMatch($TRUE) | Out-Null;
          $CertPackage.AddCheck($IcingaCheck);
       }
-   }
-
-# Check for Start of Cert
-   if(($null -ne $WarningStart) -Or ($null -ne $CriticalStart)) {
-      $CompareThumbprint = $null
-      $CertPackageStart = New-IcingaCheckPackage -Name 'Certificate Start' -OperatorAnd -Verbose $Verbosity;
-      foreach($Cert in $CertData.CertStore) {
-         if ($Cert.Thumbprint -eq $CompareThumbprint) {
-            continue;
-         }
-         $CompareThumbprint = $Cert.Thumbprint;
-         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject, $Cert.Thumbprint)) -Value (New-TimeSpan -End $Cert.NotBefore.DateTime).TotalSeconds;
-         $IcingaCheck.WarnOutOfRange((ConvertTo-SecondsFromIcingaThresholds -Threshold $WarningStart)).CritOutOfRange((ConvertTo-SecondsFromIcingaThresholds -Threshold $CriticalStart)) | Out-Null;
-	     $CertPackageStart.AddCheck($IcingaCheck);
-      }
-      foreach($Cert in $CertData.CertFile) {
-         if ($Cert.Thumbprint -eq $CompareThumbprint) {
-            continue;
-         }
-         $CompareThumbprint = $Cert.Thumbprint;
-         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject, $Cert.Thumbprint)) -Value (New-TimeSpan -End $Cert.NotAfter.DateTime).TotalSeconds;
+      if (($null -ne $WarningStart) -Or ($null -ne $CriticalStart)) {
+         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject.Substring(3).Split(",")[0], $Cert.Thumbprint)) -Value (New-TimeSpan -End $Cert.NotBefore.DateTime).TotalSeconds;
          $IcingaCheck.WarnOutOfRange((ConvertTo-SecondsFromIcingaThresholds -Threshold $WarningStart)).CritOutOfRange((ConvertTo-SecondsFromIcingaThresholds -Threshold $CriticalStart)) | Out-Null;
          $CertPackageStart.AddCheck($IcingaCheck);
       }
-   }
-# Check for End of Cert
-   if(($null -ne $WarningEnd) -Or ($null -ne $CriticalEnd)) {
-      $CompareThumbprint = $null
-      $CertPackageEnd = New-IcingaCheckPackage -Name 'Certificate End' -OperatorAnd -Verbose $Verbosity;
-      foreach($Cert in $CertData.CertStore) {
-         if ($Cert.Thumbprint -eq $CompareThumbprint) {
-            continue;
-         }
-         $CompareThumbprint = $Cert.Thumbprint;
-         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject, $Cert.Thumbprint)) -Value (New-TimeSpan -End $Cert.NotAfter.DateTime).TotalSeconds;
-         $IcingaCheck.WarnOutOfRange((ConvertTo-SecondsFromIcingaThresholds -Threshold $WarningEnd)).CritOutOfRange((ConvertTo-SecondsFromIcingaThresholds -Threshold $CriticalEnd)) | Out-Null;
-         $CertPackageEnd.AddCheck($IcingaCheck);
-         }
-      foreach($Cert in $CertData.CertFile) {
-         if ($Cert.Thumbprint -eq $CompareThumbprint) {
-            continue;
-         }
-         $CompareThumbprint = $Cert.Thumbprint;
-         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject, $Cert.Thumbprint)) -Value (New-TimeSpan -End $Cert.NotAfter.DateTime).TotalSeconds;
+      if(($null -ne $WarningEnd) -Or ($null -ne $CriticalEnd)) {
+         $IcingaCheck = New-IcingaCheck -Name ([string]::Format('Certificate {0}({1})', $Cert.Subject.Substring(3).Split(",")[0], $Cert.Thumbprint)) -Value (New-TimeSpan -End $Cert.NotAfter.DateTime).TotalSeconds;
          $IcingaCheck.WarnOutOfRange((ConvertTo-SecondsFromIcingaThresholds -Threshold $WarningEnd)).CritOutOfRange((ConvertTo-SecondsFromIcingaThresholds -Threshold $CriticalEnd)) | Out-Null;
          $CertPackageEnd.AddCheck($IcingaCheck);
       }
@@ -177,5 +128,5 @@ function Invoke-IcingaCheckCertificate()
    $CertPackage.AddCheck($CertPackageStart);
    $CertPackage.AddCheck($CertPackageEnd);
    
-   return (New-IcingaCheckResult -Name 'Certificates' -Check $CertPackage -NoPerfData $NoPerfData -Compile);
+   return (New-IcingaCheckResult -Name 'Certificates' -Check $CertPackage -NoPerfData $TRUE -Compile);
 }
