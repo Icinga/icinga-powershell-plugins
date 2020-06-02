920612af47cce38ab68aeb3701e3234749cde727
Lord Hepipud <contact@lordhepipud.de>
2020-06-02 14:27:53 +0200

    Improves fetching, resolving and filtering of user objects
    Fixes #27

--- provider/users/Get-IcingaUsers.psm1
+++ provider/users/Get-IcingaUsers.psm1
@@ -4,14 +4,40 @@ function Get-IcingaUsers()
         [array]$Username
     );
 
-    if ($null -eq $Username) {
-        return Get-LocalUser;
-    } else {
-        [array]$UserInformation = @();
-        foreach ($UniqueUser in $Username) {
-            $UserInformation += (Get-LocalUser -Name $UniqueUser);
+    $UserData          = Get-LocalUser;
+    [array]$UserOutput = @();
+
+    foreach ($user in $UserData) {
+        if ($Username.Count -ne 0) {
+            if (-Not ($Username -Contains $user.Name)) {
+                continue;
+            }
+        }
+
+        $UserOutput += @{
+            'AccountExpires'         = $user.AccountExpires;
+            'Description'            = $user.Description;
+            'Enabled'                = $user.Enabled;
+            'FullName'               = $user.FullName;
+            'PasswordChangeableDate' = $user.PasswordChangeableDate;
+            'PasswordExpires'        = $user.PasswordExpires;
+            'UserMayChangePassword'  = $user.UserMayChangePassword;
+            'PasswordRequired'       = $user.PasswordRequired;
+            'PasswordLastSet'        = $user.PasswordLastSet;
+            'LastLogon'              = $user.LastLogon;
+            'Name'                   = $user.Name;
+            'SID'                    = @{
+                'BinaryLength'     = $user.SID.BinaryLength;
+                'AccountDomainSid' = @{
+                    'BinaryLength' = $user.SID.AccountDomainSid.BinaryLength;
+                    'Value'        = $user.SID.AccountDomainSid.Value;
+                };
+                'Value'            = $user.SID.Value;
+            };
+            'PrincipalSource'        = $user.PrincipalSource;
+            'ObjectClass'            = $user.ObjectClass;
         }
     }
 
-    return $UserInformation;
+    return $UserOutput;
 }
