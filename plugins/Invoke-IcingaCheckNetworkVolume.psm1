3dc61ee1e0503351a16d869d7e1349b7fa90d6fa
Yonas Habteab <Yonas.Habteab@netways.de>
2020-08-31 14:39:40 +0200

    Add checkpackage for Mapped volume FreeSpaces

--- plugins/Invoke-IcingaCheckNetworkVolume.psm1
+++ plugins/Invoke-IcingaCheckNetworkVolume.psm1
@@ -1,14 +1,39 @@
 function Invoke-IcingaCheckNetworkVolume()
 {
     param(
-        $Critical           = $null,
-        $Warning            = $null,
-        [switch]$NoPerfData = $FALSE,
+        [array]$IncludeVolumes = @(),
+        [array]$ExcludeVolumes = @(),
+        $FreeSpaceWarning      = '20',
+        $FreeSpaceCritical     = '15',
+        [switch]$NoPerfData    = $FALSE,
         [ValidateSet(0, 1, 2)]
-        $Verbosity          = 0
+        $Verbosity             = 0
     );
 
     $CheckPackage = New-IcingaCheckPackage -Name 'Network Volumes Package' -OperatorAnd -Verbose $Verbosity;
+    $GetVolumes   = Get-IcingaNetworkVolumeData -IncludeVolumes $IncludeVolumes -ExcludeVolumes $ExcludeVolumes;
+
+    foreach ($volume in $GetVolumes.keys) {
+        $VolumeObj = $GetVolumes[$volume];
+        $VolumeCheckPackage = New-IcingaCheckPackage -Name $volume -OperatorAnd -Verbose $Verbosity;
+
+        $VolumeCheckPackage.AddCheck(
+            (
+                New-IcingaCheck `
+                    -Name ([string]::Format('vol_ {0} FreeSpace', $volume)) `
+                    -Value (
+                    Get-IcingaVolumeAvgCalculator -TotalSize $VolumeObj.Size -FreeSpace $VolumeObj.FreeSpace
+                ) `
+                    -Unit '%'
+            ).WarnIfLowerEqualThan(
+                $FreeSpaceWarning
+            ).CritIfLowerEqualThan(
+                $FreeSpaceCritical
+            )
+        );
+
+        $CheckPackage.AddCheck($VolumeCheckPackage);
+    }
 
     return (New-IcingaCheckresult -Check $CheckPackage -NoPerfData $NoPerfData -Compile);
 }
