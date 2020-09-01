05c4a06995a82d6e8d9c679b9d97f45a7598852d
Yonas Habteab <Yonas.Habteab@netways.de>
2020-09-01 17:32:06 +0200

    Add provider enums for node state and availability

--- provider/enums/Icinga_ProviderEnums.psm1
+++ provider/enums/Icinga_ProviderEnums.psm1
@@ -167,6 +167,66 @@
 }
 
 <##################################################################################################
+################# /lib/provider/culster ###########################################################
+##################################################################################################>
+
+[hashtable]$ClusterNodeDedicated = @{
+    0  = 'Not Dedicated';
+    1  = 'Unknown';
+    2  = 'Other';
+    3  = 'Storage';
+    4  = 'Router';
+    5  = 'Switch';
+    6  = 'Layer 3 Switch';
+    7  = 'Central Office Switch';
+    8  = 'Hub';
+    9  = 'Access Server';
+    10 = 'Firewall';
+    11 = 'Print';
+    12 = 'I/O';
+    13 = 'Web Caching';
+}
+
+[hashtable]$ClusterNodeDedicatedName = @{
+    'Not Dedicated'         = 0;
+    'Unknown'               = 1;
+    'Other'                 = 2;
+    'Storage'               = 3;
+    'Router'                = 4;
+    'Switch'                = 5;
+    'Layer 3 Switch'        = 6;
+    'Central Office Switch' = 7;
+    'Hub'                   = 8;
+    'Access Server'         = 9;
+    'Firewall'              = 10;
+    'Print'                 = 11;
+    'I/O'                   = 12;
+    'Web Caching'           = 13;
+}
+
+[hashtable]$ClusterPowerManagementCapabilities = @{
+    0 = 'Unknown';
+    1 = 'Not Supported';
+    2 = 'Disabled';
+    3 = 'Enabled';
+    4 = 'Power Saving Modes Entered Automatically';
+    5 = 'Power State Settable';
+    6 = 'Power Cycling Supported';
+    7 = 'Timed Power On Supported';
+}
+
+[hashtable]$ClusterPowerManagementCapabilitiesName = @{
+    'Unknown'                                  = 0;
+    'Not Supported'                            = 1;
+    'Disabled'                                 = 2;
+    'Enabled'                                  = 3;
+    'Power Saving Modes Entered Automatically' = 4;
+    'Power State Settable'                     = 5;
+    'Power Cycling Supported'                  = 6;
+    'Timed Power On Supported'                 = 7;
+}
+
+<##################################################################################################
 ################# /lib/provider/cpu ###############################################################
 ##################################################################################################>
 
@@ -697,6 +757,9 @@
     DeviceStatus                   = $DeviceStatus;
     DeviceAvailability             = $DeviceAvailability;
     DeviceAvailabilityName         = $DeviceAvailabilityName;
+    #/lib/provider/cluster
+    ClusterNodeDedicated           = $ClusterNodeDedicated;
+    ClusterNodeDedicatedName       = $ClusterNodeDedicatedName;
     #/lib/provider/cpu
     CPUArchitecture                = $CPUArchitecture;
     CPUProcessorType               = $CPUProcessorType;
