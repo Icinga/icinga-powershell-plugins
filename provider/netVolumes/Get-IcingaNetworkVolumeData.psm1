31b05d8b36ad225578ffd1970975b16e9dffa8f2
Yonas Habteab <Yonas.Habteab@netways.de>
2020-09-02 13:56:16 +0200

    Check whether a certain cmdlet command exists

--- provider/netVolumes/Get-IcingaNetworkVolumeData.psm1
+++ provider/netVolumes/Get-IcingaNetworkVolumeData.psm1
@@ -5,52 +5,58 @@ function Get-IcingaNetworkVolumeData()
         [array]$ExcludeVolumes = @()
     );
 
-    $GetSharedVolume = Get-IcingaWindowsInformation -ClassName MSCluster_ClusterSharedVolume -Namespace 'Root\MSCluster';
-    $ClusterDetails  = @{ };
+    $ClusterDetails = @{ };
 
-    foreach ($volume in $GetSharedVolume) {
-        $details    = @{'Disk' = @{}; };
-        $VolumeInfo = $volume | Select-Object -ExpandProperty 'SharedVolumeInfo';
+    try {
+        $GetSharedVolume = Get-ClusterSharedVolume;
+        if (Get-Command $GetSharedVolume -ErrorAction Stop) {
+            foreach ($volume in $GetSharedVolume) {
+                $details = @{'Disk' = @{}; };
+                $VolumeInfo = $volume | Select-Object -ExpandProperty 'SharedVolumeInfo';
 
-        if ($IncludeVolumes.Count -ne 0) {
-            if ($IncludeVolumes.Contains($volume.Name) -eq $FALSE) {
-                continue;
-            }
-        }
-
-        if ($ExcludeVolumes.Count -ne 0) {
-            if ($ExcludeVolumes.Contains($volume.Name) -eq $TRUE) {
-                continue;
-            }
-        }
-
-        $details.Add('Caption', $volume.Caption);
-        $details.Add('Description', $volume.Description);
-        $details.Add('InstallDate', $volume.InstallDate);
-        $details.Add('Name', $volume.Name);
-        $details.Add('Status', $volume.Stus);
-        $details.Add('Flags', $volume.Flags);
-        $details.Add('Characteristics', $volume.Characteristics);
-        $details.Add('VolumeName', $volume.VolumeName);
-        $details.Add('FaultState', $volume.FaultState);
-        $details.Add('VolumeOffset', $volume.VolumeOffset);
-        $details.Add('BackupState', $volume.BackupState);
-        $details.Add('PSComputerName', $volume.PSComputerName);
-
-        foreach ($item in $VolumeInfo) {
-            $details.Disk.Add(
-                $item.Name, @{
-                    'Name'        = $item.Name;
-                    'Path'        = $item.FriendlyVolumeName;
-                    'Size'        = $item.Partition.Size;
-                    'FreeSpace'   = $item.Partition.FreeSpace;
-                    'UsedSpace'   = $item.Partition.UsedSpace;
-                    'PercentFree' = $item.Partition.PercentFree;
+                if ($IncludeVolumes.Count -ne 0) {
+                    if ($IncludeVolumes.Contains($volume.Name) -eq $FALSE) {
+                        continue;
+                    }
                 }
-            );
-        }
 
-        $ClusterDetails.Add($volume.Name, $details);
+                if ($ExcludeVolumes.Count -ne 0) {
+                    if ($ExcludeVolumes.Contains($volume.Name) -eq $TRUE) {
+                        continue;
+                    }
+                }
+
+                $details.Add('Caption', $volume.Caption);
+                $details.Add('Description', $volume.Description);
+                $details.Add('InstallDate', $volume.InstallDate);
+                $details.Add('Name', $volume.Name);
+                $details.Add('Status', $volume.Stus);
+                $details.Add('Flags', $volume.Flags);
+                $details.Add('Characteristics', $volume.Characteristics);
+                $details.Add('VolumeName', $volume.VolumeName);
+                $details.Add('FaultState', $volume.FaultState);
+                $details.Add('VolumeOffset', $volume.VolumeOffset);
+                $details.Add('BackupState', $volume.BackupState);
+                $details.Add('PSComputerName', $volume.PSComputerName);
+
+                foreach ($item in $VolumeInfo) {
+                    $details.Disk.Add(
+                        $item.Name, @{
+                            'Name'        = $item.Name;
+                            'Path'        = $item.FriendlyVolumeName;
+                            'Size'        = $item.Partition.Size;
+                            'FreeSpace'   = $item.Partition.FreeSpace;
+                            'UsedSpace'   = $item.Partition.UsedSpace;
+                            'PercentFree' = $item.Partition.PercentFree;
+                        }
+                    );
+                }
+
+                $ClusterDetails.Add($volume.Name, $details);
+            }
+        }
+    } catch {
+        Exit-IcingaThrowException -ExceptionType 'Custom' -CustomMessage 'NotFoundError' -InputString 'The classes used for the plugin could not be found on your server';
     }
 
     return $ClusterDetails;
