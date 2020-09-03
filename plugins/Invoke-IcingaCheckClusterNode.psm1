0beffa8fd06a81d53c195dc63fd701d81e4d3292
Yonas Habteab <Yonas.Habteab@netways.de>
2020-09-03 17:17:10 +0200

    Add first CheckPackage for testing purposes

--- plugins/Invoke-IcingaCheckClusterNode.psm1
+++ plugins/Invoke-IcingaCheckClusterNode.psm1
@@ -10,4 +10,22 @@ function Invoke-IcingaCheckClusterNode()
 
     $CheckPackage    = New-IcingaCheckPackage -Name 'Cluster Nodes Package' -OperatorAnd -Verbose $Verbosity;
     $GetClusterNodes = Get-IcingaClusterNodeInfo;
+
+    foreach ($node in $GetClusterNodes.Keys) {
+        $ClusterNode = $GetClusterNodes[$node];
+        $NodeCheckPackage = New-IcingaCheckPackage -Name ([string]::Format('Node {0}', $ClusterNode.Name)) -OperatorAnd -Verbose $Verbosity;
+
+        $NodeCheckPackage.AddCheck(
+            (
+                New-IcingaCheck `
+                    -Name ([string]::Format('{0} Status', $ClusterNode.Name)) `
+                    -Value $ClusterNode.Status `
+                    -NoPerfData
+            )
+        );
+
+        $CheckPackage.AddCheck($NodeCheckPackage);
+    }
+
+    return (New-IcingaCheckresult -Check $CheckPackage -NoPerfData $NoPerfData -Compile);
 }
