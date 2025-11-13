5e54d3f6075b0ea3c3b1677eca13a36baaf86365
Lord Hepipud <contact@lordhepipud.de>
2025-11-13 17:04:34 +0100

    Improves size detection for user quotas
    Fixes partition total size collection by properly ignoring possible user quotas

--- provider/disks/Get-IcingaPartitionSpace.psm1
+++ provider/disks/Get-IcingaPartitionSpace.psm1
@@ -4,21 +4,45 @@ function Get-IcingaPartitionSpace()
         [int]$DriveType = 3
     );
 
-    [array]$Volumes = Get-IcingaWindowsInformation Win32_Volume -Filter ([string]::Format('DriveType = "{0}"', $DriveType));
+    [array]$Volumes      = Get-IcingaWindowsInformation Win32_Volume -Filter ([string]::Format('DriveType = "{0}"', $DriveType));
+    # Lets store all partition data, required to get actual partition sizes ignoring user quotas
+    [array]$Partitions   = Get-Partition;
     [hashtable]$DiskData = @{ };
 
     foreach ($disk in $Volumes) {
-        if ($DiskData.ContainsKey($disk.DeviceID)) {
+        if ([string]::IsNullOrEmpty($disk.DriveLetter)) {
             continue;
         }
 
+        if ($DiskData.ContainsKey($disk.DriveLetter)) {
+            continue;
+        }
+
+        # Create a fallback value - shouldn't apply anyway
+        [decimal]$DiskSize = $disk.Capacity;
+
+        # Now loop through all our partitions for the partition with our drive letter and
+        # get the actual size from there, ignoring possible user quotas
+        foreach ($partition in $Partitions) {
+            if ([string]::IsNullOrEmpty($partition.DriveLetter)) {
+                continue;
+            }
+
+            if ($partition.DriveLetter -ne $disk.DriveLetter.Replace(':', '').Replace('\', '')) {
+                continue;
+            }
+
+            $DiskSize = [decimal]$partition.Size;
+            break;
+        }
+
         $DiskData.Add(
-            $disk.DeviceID,
+            $disk.DriveLetter,
             @{
-                'Size'        = $disk.Capacity;
+                'Size'        = $DiskSize;
                 'FreeSpace'   = $disk.FreeSpace;
-                'UsedSpace'   = ($disk.Capacity - $disk.FreeSpace);
-                'DriveLetter' = $disk.DeviceID;
+                'UsedSpace'   = ($DiskSize - $disk.FreeSpace);
+                'DriveLetter' = $disk.DriveLetter;
             }
         );
     }
