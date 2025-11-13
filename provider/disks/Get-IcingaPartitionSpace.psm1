168035e3bc58cdc26c58fcf9545ff432617351f3
Thomas Widhalm <widhalmt@widhalm.or.at>
2025-11-13 17:13:46 +0100

    Change checked Disk from LogicalDisk to Volume (#453)
    * Change checked Disk from LogicalDisk to Volume
    
    The reason behind the change is that it seems that `LogicalVolume` will partly consider disk quotas and partly ignore them. We had an issue where a customer reported negative values for the calculated free space in case they had quotas for the checking user in place. Looks like `Size` of a `LogicalDisk` gives the size of the quota and `FreeSpace` ignores quota.
    So I changed the queried classes to hope for total values without queries.
    
    @LordHepipud : Please feel free to correct me if I went the wrong path. And: I can think of reasons why you used `LogicalDisk` instead of `Volume`, so if there's anything I missed, please feel free to just make "User running the plugin must not have a quota applied" a requirement and throw away my PR.
    
    Some things to note about this PR:
    
    * @RincewindsHat had the initial idea about what to look for
    * I did something that I would *never* advise someone to do but I checked *first* with the maintainer if it's ok as an exception (details follow)
    * I don't know how to code PowerShell
    * I used an LLM to help me with identifying the new code snippets I needed to use
    * I don't have a way to check the code because I don't use Windows
    
    * Improves size detection for user quotas
    
    Fixes partition total size collection by properly ignoring possible user quotas
    
    * Updates docs
    
    * Fixes changelog
    
    ---------
    
    Co-authored-by: Lord Hepipud <contact@lordhepipud.de>

--- provider/disks/Get-IcingaPartitionSpace.psm1
+++ provider/disks/Get-IcingaPartitionSpace.psm1
@@ -4,21 +4,45 @@ function Get-IcingaPartitionSpace()
         [int]$DriveType = 3
     );
 
-    [array]$LogicalDisks = Get-IcingaWindowsInformation Win32_LogicalDisk -Filter ([string]::Format('DriveType = "{0}"', $DriveType));
+    [array]$Volumes      = Get-IcingaWindowsInformation Win32_Volume -Filter ([string]::Format('DriveType = "{0}"', $DriveType));
+    # Lets store all partition data, required to get actual partition sizes ignoring user quotas
+    [array]$Partitions   = Get-Partition;
     [hashtable]$DiskData = @{ };
 
-    foreach ($disk in $LogicalDisks) {
-        if ($DiskData.ContainsKey($disk.DeviceID)) {
+    foreach ($disk in $Volumes) {
+        if ([string]::IsNullOrEmpty($disk.DriveLetter)) {
             continue;
         }
 
+        if ($DiskData.ContainsKey($disk.DriveLetter)) {
+            continue;
+        }
+
+        # Create a fallback value - shouldn't apply anyway
+        [decimal]$DiskSize = $disk.Capacity;
+
+        # Now loop through all our partitions for the partition with our drive letter and
+        # get the actual size from there, ignoring possible user quotas
+        foreach ($partition in $Partitions) {
+            if ([string]::IsNullOrEmpty($partition.DriveLetter)) {
+                continue;
+            }
+
+            if ($partition.DriveLetter -ne $disk.DriveLetter.Replace(':', '').Replace('\', '')) {
+                continue;
+            }
+
+            $DiskSize = [decimal]$partition.Size;
+            break;
+        }
+
         $DiskData.Add(
-            $disk.DeviceID,
+            $disk.DriveLetter,
             @{
-                'Size'        = $disk.Size;
+                'Size'        = $DiskSize;
                 'FreeSpace'   = $disk.FreeSpace;
-                'UsedSpace'   = ($disk.Size - $disk.FreeSpace);
-                'DriveLetter' = $disk.DeviceID;
+                'UsedSpace'   = ($DiskSize - $disk.FreeSpace);
+                'DriveLetter' = $disk.DriveLetter;
             }
         );
     }
